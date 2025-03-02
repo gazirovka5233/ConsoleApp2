33e1addba14d20e42a092d5a48f1e1b9e3f83336
gazirovka5233 <bayk2006@mail.ru>
2025-03-02 10:46:38 +0700

    Добавлен новый класс Calculator

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+ public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
